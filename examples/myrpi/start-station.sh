5d1401f4f76c868246f47e056ca2a671c5dbaeff
Anton Bezemsky <antonbezemsky@yandex.ru>
2024-04-02 15:07:12 +0300

    Add myrpi example

--- examples/myrpi/start-station.sh
+++ examples/myrpi/start-station.sh
@@ -82,7 +82,7 @@ STATION_BIN="../../build-rpi-$variant/bin/station"
 if [ -f "$STATION_BIN" ]; then
 	printf "Using variant=$variant, lns_config='$lns_config'\n"
 	printf "$GREEN Starting Station ... $NC\n"
-	rinit.sh
+	./rinit.sh
 	$STATION_BIN -h $lns_config
 else
 	printf "$RED [ERROR]: Binary not found @ $STATION_BIN $NC\n"
